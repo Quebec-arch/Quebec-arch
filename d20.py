605c58004eacb002828dea969d9ae8059a67dacd
Quebec-arch <pedro.rodriguesldl19@gmail.com>
2024-09-05 21:56:33 -0300

    Add files via upload
    Some basic D20

---
+++ d20.py
@@ -0,0 +1,14 @@
+#Criar um D20 em Python
+from random import randint
+from time import sleep
+
+print('=.=' * 20)
+dado = input('Press enter to roll the dice ')
+nat = randint(1, 20)
+print('Rolling')
+sleep(2)
+print(nat)
+if nat == 1:
+    print('\033[31mCritical Fail\033[m')
+if nat == 20:
+    print('\033[34mCritical Success\033[m')
